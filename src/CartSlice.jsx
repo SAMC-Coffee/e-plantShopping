6ec4ba5bf0bdc34b32656a236e05627cc45f2d19
SAMC-Coffee <daemonsam666@gmail.com>
2026-03-31 11:16:51 -0500

    Revert "Completed project requirements"
    This reverts commit 6c9e1f7be818685b8b085677aa236897778a4361.

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -3,32 +3,12 @@ import { createSlice } from '@reduxjs/toolkit';
 export const CartSlice = createSlice({
 	name: 'cart',
 	initialState: {
-		items: [], // Initialize items as an empty array
-		totalItems: 0
+		items: [] // Initialize items as an empty array
 	},
 	reducers: {
-		addItem: (state, action) => {
-			const {name, image, cost} = action.payload;
-			const existingItem = state.items.find((item) => item.name === name);
-			if (existingItem) {
-				existingItem.quantity++;
-			}
-			else {
-				state.items.push({name, image, cost, quantity: 1});
-			}
-			state.totalItems++;
-		},
-		removeItem: (state, action) => {
-			state.items = state.items.filter((item) => item.name !== action.payload);
-			state.totalItems = 0;
-		},
-		updateQuantity: (state, action) => {
-			const {name, quantity} = action.payload;
-			const itemToUpdate = state.items.find((item) => item.name === name);
-			if (itemToUpdate) {
-				itemToUpdate.quantity = quantity;
-			}
-		}
+		addItem: (state, action) => {},
+		removeItem: (state, action) => {},
+		updateQuantity: (state, action) => {}
 	}
 });
 
